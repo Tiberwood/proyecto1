4146ab6eec95280972f5776fbb17e81068194090
Belen Muñoz <velero_chopin@MacBook-Pro-de-Belen.local>
2020-08-19 22:13:22 -0400

    UserSerializer all fields removed

--- app/serializers/UserSerializer.py
+++ app/serializers/UserSerializer.py
@@ -8,7 +8,6 @@ class UserSerializer(ModelSerializer):
 
     class Meta:
         model = User
-        #fields = '__all__'
         fields = ('id', 'first_name', 'last_name', 'email', 'groups')
 
     def get_groups(self, user):
